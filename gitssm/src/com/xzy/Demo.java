2bf9bb70bc7a6ccec938a9887ef6067ee3f9ec5f
“javaxiaoxu” <1164395121@qq.com>
2019-10-17 15:33:05 +0800

    git send local repository

--- gitssm/src/com/xzy/Demo.java
+++ gitssm/src/com/xzy/Demo.java
@@ -4,12 +4,10 @@ package com.xzy;
  * @author XZY
  * @site www.Xzy.com
  * @company
- * @create  2019-10-17 15:16
+ * @create  2019-10-17 15:31
  */
 public class Demo {
-
     public static void main(String[] args) {
         System.out.println("git idea test ");
     }
-
 }
