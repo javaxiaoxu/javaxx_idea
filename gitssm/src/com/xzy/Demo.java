1b90c1d4ac8b1e200e2914367f1d025b6011f0db
“javaxiaoxu” <1164395121@qq.com>
2019-10-17 15:58:00 +0800

    javaxzy

--- gitssm/src/com/xzy/Demo.java
+++ gitssm/src/com/xzy/Demo.java
@@ -10,5 +10,6 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("git idea test update ");
         System.out.println("emmmmmmmmmmmm");
+        System.out.println("javaxzy");
     }
 }
